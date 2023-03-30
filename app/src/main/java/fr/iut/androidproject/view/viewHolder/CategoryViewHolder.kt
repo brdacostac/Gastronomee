d5724987a77dab03f89e8eb78eb385cb49d93d9c
Bruno Da Costa Cunha <Bruno.DA_COSTA_CUNHA@etu.uca.fr>
2023-03-30 14:50:05 +0200

    Images api convertor marchent

--- app/src/main/java/fr/iut/androidproject/view/viewHolder/CategoryViewHolder.kt
+++ app/src/main/java/fr/iut/androidproject/view/viewHolder/CategoryViewHolder.kt
@@ -1,8 +1,10 @@
 package fr.iut.androidproject.view.viewHolder
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.squareup.picasso.Picasso
 import fr.iut.androidproject.R
 import fr.iut.androidproject.model.Category
 import fr.iut.androidproject.model.Recette
@@ -10,10 +12,10 @@ import fr.iut.androidproject.model.Recette
 
 class CategoryViewHolder(itemView: View) : ViewHolder(itemView){
     private val nomTextView: TextView = itemView.findViewById(R.id.nomCategory)
-    private val imageTextView: TextView = itemView.findViewById(R.id.imageCategory)
+    private val imageTextView: ImageView = itemView.findViewById(R.id.imageCategory)
 
     fun bind(category: Category) {
         nomTextView.text = category.nom
-        imageTextView.text = category.image
+        Picasso.get().load(category.image).into(imageTextView)
     }
 }
