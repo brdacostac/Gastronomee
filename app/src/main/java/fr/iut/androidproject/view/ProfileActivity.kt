ef3a4c6c15cb380bb9fe477bdad023d896e9b378
Hugo PRADIER <Hugo.PRADIER2@etu.uca.fr>
2023-04-08 16:31:36 +0200

    prise photo fonctionnelle

--- app/src/main/java/fr/iut/androidproject/view/ProfileActivity.kt
+++ app/src/main/java/fr/iut/androidproject/view/ProfileActivity.kt
@@ -5,9 +5,7 @@ import android.app.Activity
 import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.media.MediaScannerConnection
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -21,10 +19,9 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
+import com.squareup.picasso.Picasso
 import fr.iut.androidproject.R
 import java.io.File
-import java.io.FileInputStream
-import java.io.FileOutputStream
 import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
@@ -33,6 +30,8 @@ class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        imageView = findViewById<ImageView>(R.id.profile_image)
 
         val userFullname = intent.getStringExtra("USER_FULLNAME")
         val userUsername = intent.getStringExtra("USER_USERNAME")
@@ -51,7 +50,6 @@ class ProfileActivity : AppCompatActivity() {
                 saveImageToGallery(photoFile.absolutePath)
             }
         }
-
     }
 
     private val REQUEST_IMAGE_CAPTURE = 1
@@ -61,12 +59,12 @@ class ProfileActivity : AppCompatActivity() {
 
     private lateinit var takePictureLauncher: ActivityResultLauncher<Intent>
 
-    fun onTakePhotoClicked(view: View) {
-        val permission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+    fun takePhoto(view: View) {
+        val permission = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
         if (permission != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(
                 this,
-                arrayOf(Manifest.permission.CAMERA),
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
                 REQUEST_IMAGE_CAPTURE
             )
         } else {
@@ -74,13 +72,17 @@ class ProfileActivity : AppCompatActivity() {
         }
     }
 
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            dispatchTakePictureIntent()
+        }
+    }
+
     private fun dispatchTakePictureIntent() {
         Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
-            // Ensure that there's a camera activity to handle the intent
             takePictureIntent.resolveActivity(packageManager)?.also {
-                // Create the File where the photo should go
                 photoFile = createImageFile()
-                // Continue only if the File was successfully created
                 photoFile.also {
                     val photoURI: Uri = FileProvider.getUriForFile(
                         this,
@@ -97,18 +99,14 @@ class ProfileActivity : AppCompatActivity() {
 
     @Throws(IOException::class)
     private fun createImageFile(): File {
-        // Create an image file name
-        val timeStamp: String =
-            SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
         val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
         return File.createTempFile(
-            "JPEG_${timeStamp}_", /* prefix */
-            ".jpg", /* suffix */
-            storageDir /* directory */
+            "JPEG_${timeStamp}_",
+            ".jpg",
+            storageDir
         ).apply {
-            // Save a file: path for use with ACTION_VIEW intents
             val savedImagePath = absolutePath
-            imageView = findViewById(R.id.profile_image)
             imageView.setImageBitmap(BitmapFactory.decodeFile(savedImagePath))
         }
     }
@@ -123,18 +121,11 @@ class ProfileActivity : AppCompatActivity() {
         val externalUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
         val contentResolver = applicationContext.contentResolver
 
-        // Insert the image into the MediaStore
         val uri = contentResolver.insert(externalUri, values)
 
-        // Save the image to the MediaStore
-        val outputStream = contentResolver.openOutputStream(uri!!)
-        val inputStream = FileInputStream(photoFile)
-        outputStream?.use { out ->
-            inputStream.use { input ->
-                input.copyTo(out)
-            }
-        }
-        // Afficher l'image sauvegardée
-        findViewById<ImageView>(R.id.profile_image).setImageURI(uri)
+        Picasso.get()
+            .load(uri)
+            .into(imageView)
+        imageView.setImageBitmap(BitmapFactory.decodeFile(savedImagePath))
     }
 }
